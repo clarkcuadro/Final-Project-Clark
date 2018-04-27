b34f4c7e28ffcd217b062be8788fcb7f1e71510a
CM Student <cmstudent@uhwo-acm-09.local>
2018-04-27 10:56:21 -1000

    added annotations

--- HackwichThree/mapVCViewController.swift
+++ HackwichThree/mapVCViewController.swift
@@ -15,17 +15,38 @@ class mapVCViewController: UIViewController {
     //set initial location to UHWO
     let initialLocation = CLLocation(latitude: 21.361888 , longitude: -158.055725)
     //set region around UHWO to 1000m
-    let regionRadius: CLLocationDistance = 1000
+    let regionRadius: CLLocationDistance = 10000
+    
+    
+    
+    
     
     override func viewDidLoad() {
         
         centerMapOnLocation(location: initialLocation)
         
         //create an Restaurant object and set all the arguments to each of the three parameters
-        let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate:
+        let restaurantOne = Restaurant(title: "American", type: "Kalapawai", coordinate:
             CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
         mapView.addAnnotation(restaurantOne)
         super.viewDidLoad()
+        
+        let restaurantTwo = Restaurant(title: "Japanese", type: "Sushi Bay", coordinate: CLLocationCoordinate2D(latitude: 21.3398, longitude: -158.0782))
+        mapView.addAnnotation(restaurantTwo)
+        
+        let restaurantThree = Restaurant(title: "Mexican", type: "El Fuego", coordinate: CLLocationCoordinate2D(latitude: 21.3445, longitude: -158.1156))
+        mapView.addAnnotation(restaurantThree)
+        
+        let restaurantFour = Restaurant(title: "Japanese", type: "Agu Ramen", coordinate: CLLocationCoordinate2D(latitude: 21.3382, longitude: -158.0809))
+        mapView.addAnnotation(restaurantFour)
+        
+        let restaurantFive = Restaurant(title: "Filipino", type: "Julie'z", coordinate: CLLocationCoordinate2D(latitude: 21.3380, longitude: -158.0804))
+        mapView.addAnnotation(restaurantFive)
+        
+        let restaurantSix = Restaurant(title: "Japanese", type: "Okome Hawaii", coordinate: CLLocationCoordinate2D(latitude: 21.3336, longitude: -158.0517))
+        mapView.addAnnotation(restaurantSix)
+        
+        
         // Do any additional setup after loading the view.
     }
     func centerMapOnLocation(location: CLLocation){
